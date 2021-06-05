cc3273d4dc801b313a86a674ff377d9da547f243
Rudenko Ivan <rudenko_ia@outlook.com>
2021-06-05 14:00:30 +0900

    Add data layer

---
+++ database/migrations/2021_06_05_044515_article.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Article extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->boolean('published');
+            $table->boolean('deleted');
+            //$table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+        //
+    }
+}
---
+++ database/migrations/2021_06_05_045607_articleCategory.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ArticleCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ArticleCategory', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('article_id');
+            $table->bigInteger('category_id');
+            $table->unique(['article_id','category_id']);
+            //$table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
